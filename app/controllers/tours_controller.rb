17a9673eef411b0d55cd080568120728ec75d361
dungtm-1343 <tran.manh.dung@sun-asterisk.com>
2024-05-24 12:31:29 +0700

    Search and Filter

--- app/controllers/tours_controller.rb
+++ app/controllers/tours_controller.rb
@@ -1,6 +1,50 @@
 class ToursController < ApplicationController
+  before_action :search_upcoming, :filter_tours, only: :index
+
   def index
-    @pagy, @tours = pagy(Tour.upcoming,
-                         items: Settings.tour.items_per_page)
+    @pagy, @tours = pagy(@tours, items: Settings.tour.items_per_page)
+  end
+
+  private
+
+  def search_upcoming
+    @tours = Tour.upcoming
+  end
+
+  def search_by_attr field
+    if Tour::RANGE_ATTRIBUTES.include? field
+      method_min = "by_min_#{field}"
+      method_max = "by_max_#{field}"
+      min_value = params["min_#{field}"]
+      max_value = params["max_#{field}"]
+      @tours = @tours.public_send method_min, min_value if min_value.present?
+      @tours = @tours.public_send method_max, max_value if max_value.present?
+    else
+      param = params[field]
+      method = "by_#{field}"
+      @tours = @tours.public_send method, param if param.present?
+    end
+  end
+
+  def search_with_and
+    Tour::SEARCH_ATTRIBUTES.each{|field| search_by_attr field}
+  end
+
+  def search_with_or
+    tours = []
+    Tour::SEARCH_ATTRIBUTES.each do |field|
+      @tours = Tour.upcoming
+      search_by_attr field
+      tours += @tours
+    end
+    @tours = Tour.where(id: tours.map(&:id)).distinct
+  end
+
+  def filter_tours
+    if params[:search_type]
+      search_with_or
+    else
+      search_with_and
+    end
   end
 end
--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,4 +1,30 @@
 class Tour < ApplicationRecord
+  TOUR_ATTRIBUTES = %i(
+    tour_name
+    city
+    tour_destination
+    description
+    price
+    day_duration
+    start_date
+    end_date
+    tour_type_id
+    image
+  ).freeze
+
+  SEARCH_ATTRIBUTES = %i(
+    name
+    city
+    start_date
+    duration
+    price
+  ).freeze
+
+  RANGE_ATTRIBUTES = %i(
+    duration
+    price
+  ).freeze
+
   belongs_to :tour_type
   has_many :tour_flights, dependent: :destroy
   has_many :flights, through: :tour_flights
@@ -11,20 +37,45 @@ class Tour < ApplicationRecord
   validates :day_duration, presence: true,
                        numericality: {only_integer: true,
                                       maximum: Settings.tour.max_duration}
-  validates :start_date, presence: true
-  validates :end_date, presence: true
+  validates :start_date, :end_date, presence: true
   validate :end_date_after_start_date
+  validate :start_date_after_today
 
-  has_one_attached :image do |attachable|
+  has_many_attached :image do |attachable|
     attachable.variant :display, resize_to_limit: Settings.tour.limit_250_250
   end
 
   scope :upcoming, ->{order start_date: :asc}
+  scope :by_name,
+        ->(name){where("tour_name LIKE ?", "%#{name}%")}
+  scope :by_min_duration,
+        ->(min_duration){where("day_duration >= ?", min_duration.presence)}
+  scope :by_max_duration,
+        ->(max_duration){where("day_duration <= ?", max_duration)}
+  scope :by_city,
+        ->(city){where("city LIKE ?", "%#{city}%")}
+  scope :by_min_price,
+        ->(min_price){where("price >= ?", min_price)}
+  scope :by_max_price,
+        ->(max_price){where("price <= ?", max_price)}
+  scope :by_start_date,
+        ->(start_date){where("start_date > ?", start_date)}
 
   private
+
   def end_date_after_start_date
+    return if end_date.blank? || start_date.blank?
+
     return unless end_date <= start_date
 
-    errors.add(:end_date, t("end_day_start_date"))
+    errors.add(:end_date, I18n.t("errors.end_day_start_date"))
+  end
+
+  def start_date_after_today
+    return if start_date.blank?
+
+    return unless start_date <= Time.zone.today
+
+    errors.add(:start_date, I18n.t("errors.start_date_today"))
   end
 end
